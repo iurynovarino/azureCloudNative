27ffc0a6d604ec85c386d618c9b54f990a4e32e2
iury <novarinos@hotmail.com>
2025-05-22 20:38:08 -0300

    Estrutura de pasta atualizada

--- lab3/scripts.ps1
+++ lab3/scripts.ps1
@@ -1,16 +1,14 @@
 docker build -t blog-iury-app:latest .
-
-docker run -d -p 80:80 blog-iury-app:latest
  
-#Login na Azure
+docker run -d -p 80:80 blog-iury-app:latest
+
 az login
  
 # Create a resource group
 az group create --name containerappslab03 --location eastus
  
 # Create Container Registry
-az acr create --resource-group containerappslab03 --name blogiuryacr --sku Basic
-
+az acr create --resource-group containerappslab03 --name blogiury --sku Basic
 # Login to ACR
 az acr login --name blogheblogiuryacr
 
@@ -22,20 +20,19 @@ docker push blogiuryacr.azurecr.io/blog-iury-app:latest
 
 #containerID = blogiuryacr.azurecr.io/blog-iury-app:latest
 #user = blogiuryacr
-#password = duzVmne8ARNgFRMndpcI7910kqjO3fczyj7zUDyAxQ4ARCFXf2B 
+#password = duzVmne8ARNgFRMndpcI7910kqjO3fczyj7zUDyAxQ4ARCFXf2B #Essa senha é obtida no conteiner registry acess key.
 
 # Create Environment container app
 az containerapp env create --name blog-iury-env --resource-group containerapps001 --location eastus
 
 az containerapp create \
-  --name blog-henrique-app \
+  --name blog-iury-app \
   --resource-group containerapps001 \
   --location eastus \
-  --image bloghenriqueusr.azurecr.io/blog-henrique-app:latest \
-  --environment blog-henrique-env \
+  --image blogiuryusr.azurecr.io/blog-iury-app:latest \
+  --environment blog-iury-env \
   --target-port 80 \
   --ingress external \
-  --registry-username bloghenriqueusr \
+  --registry-username blogiuryusr \
   --registry-password xhN0nfwmgPrRmdexZBlokagQ9YT4jvFDybqA4CRFqfxB \
-  --registry-server bloghenriqueusr.azurecr.io
-
+  --registry-server blogiuryusr.azurecr.io
